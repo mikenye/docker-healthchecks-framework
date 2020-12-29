ebbbf2f549e069adec836d21b4fd61d3f22ad82d
Mike <mike.nye@gmail.com>
2020-12-29 18:51:04 +0800

    change SCRIPTPATH to be more reliable

--- checks/check_tcp4_socket_listening.sh
+++ checks/check_tcp4_socket_listening.sh
@@ -6,7 +6,7 @@ function check_tcp4_socket_listening() {
     # ----
     
     # source common regexes
-    SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+    SCRIPTPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
     . "$SCRIPTPATH/../common/common_regex_patterns.sh"
     
     # Check local IP input
